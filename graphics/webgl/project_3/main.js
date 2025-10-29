c194903438e5c7cf8ed52b9a50dda0dced250d24
Exiivy <jscadman3564@gmail.com>
2025-10-29 10:13:50 +0900

    initial commit

---
+++ graphics/webgl/project_3/main.js
@@ -0,0 +1,332 @@
+import { resizeAspectRatio, setupText, Axes } from './util/util.js';
+import { Shader, readShaderFile } from './util/shader.js';
+
+// Global variables
+const canvas = document.getElementById('glCanvas');
+const gl = canvas.getContext('webgl2');
+const SHAPE = {
+    CIRCLE: 0, LINE: 1, INVALID: 2
+};
+const CIRCLE_PRECISION = 200;
+
+let isInitialized = false;  // main이 실행되는 순간 true로 change
+let shader;
+let vao;
+let positionBuffer; // 2D position을 위한 VBO (Vertex Buffer Object)
+let isDrawing = false; // mouse button을 누르고 있는 동안 true로 change
+let shape = 0; // 0 = circle, 1 = line, else = null. See const SHAPE
+let startPoint = null;  // mouse button을 누른 위치
+let tempEndPoint = null; // mouse를 움직이는 동안의 위치
+let lines = []; // 그려진 선분들을 저장하는 array
+let circles = [];
+let intersections = [];
+let axes = new Axes(gl, 0.85); // x, y axes 그려주는 object (see util.js)
+
+// DOMContentLoaded event
+// 1) 모든 HTML 문서가 완전히 load되고 parsing된 후 발생
+// 2) 모든 resource (images, css, js 등) 가 완전히 load된 후 발생
+// 3) 모든 DOM 요소가 생성된 후 발생
+// DOM: Document Object Model로 HTML의 tree 구조로 표현되는 object model 
+// 모든 code를 이 listener 안에 넣는 것은 mouse click event를 원활하게 처리하기 위해서임
+// mouse input을 사용할 때 이와 같이 main을 call 한다. 
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (isInitialized) { // true인 경우는 main이 이미 실행되었다는 뜻이므로 다시 실행하지 않음
+        console.log("Already initialized");
+        return;
+    }
+
+    main().then(success => { // call main function
+        if (!success) {
+            console.log('프로그램을 종료합니다.');
+            return;
+        }
+        isInitialized = true;
+    }).catch(error => {
+        console.error('프로그램 실행 중 오류 발생:', error);
+    });
+});
+
+function initWebGL() {
+    if (!gl) {
+        console.error('WebGL 2 is not supported by your browser.');
+        return false;
+    }
+
+    canvas.width = 700;
+    canvas.height = 700;
+
+    resizeAspectRatio(gl, canvas);
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.1, 0.2, 0.3, 1.0);
+
+    return true;
+}
+
+function setupBuffers() {
+    vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    shader.setAttribPointer('a_position', 2, gl.FLOAT, false, 0, 0); // x, y 2D 좌표
+
+    gl.bindVertexArray(null);
+}
+
+// 좌표 변환 함수: 캔버스 좌표를 WebGL 좌표로 변환
+// 캔버스 좌표: 캔버스 좌측 상단이 (0, 0), 우측 하단이 (canvas.width, canvas.height)
+// WebGL 좌표 (NDC): 캔버스 좌측 하단이 (-1, -1), 우측 상단이 (1, 1)
+function convertToWebGLCoordinates(x, y) {
+    return [
+        (x / canvas.width) * 2 - 1,  // x/canvas.width 는 0 ~ 1 사이의 값, 이것을 * 2 - 1 하면 -1 ~ 1 사이의 값
+        -((y / canvas.height) * 2 - 1) // y canvas 좌표는 상하를 뒤집어 주어야 하므로 -1을 곱함
+    ];
+}
+
+/* 
+    browser window
+    +----------------------------------------+
+    | toolbar, address bar, etc.             |
+    +----------------------------------------+
+    | browser viewport (컨텐츠 표시 영역)       | 
+    | +------------------------------------+ |
+    | |                                    | |
+    | |    canvas                          | |
+    | |    +----------------+              | |
+    | |    |                |              | |
+    | |    |      *         |              | |
+    | |    |                |              | |
+    | |    +----------------+              | |
+    | |                                    | |
+    | +------------------------------------+ |
+    +----------------------------------------+
+
+    *: mouse click position
+
+    event.clientX = browser viewport 왼쪽 경계에서 마우스 클릭 위치까지의 거리
+    event.clientY = browser viewport 상단 경계에서 마우스 클릭 위치까지의 거리
+    rect.left = browser viewport 왼쪽 경계에서 canvas 왼쪽 경계까지의 거리
+    rect.top = browser viewport 상단 경계에서 canvas 상단 경계까지의 거리
+
+    x = event.clientX - rect.left  // canvas 내에서의 클릭 x 좌표
+    y = event.clientY - rect.top   // canvas 내에서의 클릭 y 좌표
+*/
+
+function setupMouseEvents() {
+    function handleMouseDown(event) {
+        event.preventDefault(); // 이미 존재할 수 있는 기본 동작을 방지
+        event.stopPropagation(); // event가 상위 요소 (div, body, html 등)으로 전파되지 않도록 방지
+
+        const rect = canvas.getBoundingClientRect(); // canvas를 나타내는 rect 객체를 반환
+        const x = event.clientX - rect.left;  // canvas 내 x 좌표
+        const y = event.clientY - rect.top;   // canvas 내 y 좌표
+        
+        if (!isDrawing) {
+            if(shape == SHAPE.CIRCLE || shape == SHAPE.LINE){
+                let [glX, glY] = convertToWebGLCoordinates(x, y);
+                startPoint = [glX, glY];
+                isDrawing = true; // 이제 mouse button을 놓을 때까지 계속 true로 둠. 즉, mouse down 상태가 됨
+            }
+        }
+    }
+
+    function handleMouseMove(event) {
+        if (isDrawing) {
+            if(shape == SHAPE.CIRCLE || shape == SHAPE.LINE){
+                const rect = canvas.getBoundingClientRect();
+                const x = event.clientX - rect.left;
+                const y = event.clientY - rect.top;
+                
+                let [glX, glY] = convertToWebGLCoordinates(x, y);
+                tempEndPoint = [glX, glY];
+                render();
+            }
+        }
+    }
+
+    function handleMouseUp() {
+        if (isDrawing && tempEndPoint) {
+
+            // lines.push([...startPoint, ...tempEndPoint])
+            //   : startPoint와 tempEndPoint를 펼쳐서 하나의 array로 합친 후 lines에 추가
+            // ex) lines = [] 이고 startPoint = [1, 2], tempEndPoint = [3, 4] 이면,
+            //     lines = [[1, 2, 3, 4]] 이 됨
+            // ex) lines = [[1, 2, 3, 4]] 이고 startPoint = [5, 6], tempEndPoint = [7, 8] 이면,
+            //     lines = [[1, 2, 3, 4], [5, 6, 7, 8]] 이 됨
+
+            if (shape == SHAPE.CIRCLE) {
+                circles.push([...startPoint, ...tempEndPoint]); 
+                setupText(canvas, "Circle: center (" + circles[0][0].toFixed(2) + ", " + circles[0][1].toFixed(2) +
+                    ") radius = " + Math.sqrt(Math.pow((circles[0][0] - circles[0][2]), 2) + Math.pow(circles[0][1] - circles[0][3], 2)).toFixed(2), 1);
+                shape = SHAPE.LINE;
+            }
+            else if (shape == SHAPE.LINE){
+                lines.push([...startPoint, ...tempEndPoint]); 
+                setupText(canvas, "Line segment: (" + lines[0][0].toFixed(2) + ", " + lines[0][1].toFixed(2) + 
+                    ") ~ (" + lines[0][2].toFixed(2) + ", " + lines[0][3].toFixed(2) + ")", 2);
+                shape = SHAPE.INVALID;
+
+                // Calculate intersections here!
+                let a = lines[0][2] - lines[0][0];
+                let b = lines[0][0];
+                let c = lines[0][3] - lines[0][1];
+                let d = lines[0][1];
+                let e = circles[0][0];
+                let f = circles[0][1];
+                let r = Math.sqrt(Math.pow((circles[0][0] - circles[0][2]), 2) + Math.pow(circles[0][1] - circles[0][3], 2));
+                let A = Math.pow(a, 2) + Math.pow(c, 2);
+                let B = 2 * (a * b - a * e + c * d - c * f);
+                let C = Math.pow(b, 2) + Math.pow(d, 2) + Math.pow(e, 2) + Math.pow(f, 2) - Math.pow(r, 2) - 2 * (b * e + d * f);
+
+                let D = Math.pow(B, 2) - 4 * A * C;
+
+                let num_t = 0; let t1, t2;
+                if(D < 0){ num_t = 0; }
+                else if(D == 0){
+                    t1 = -B / (2 * A);
+
+                    if(t1 >= 0 && t1 <= 1){ num_t++; intersections.push([a * t1 + b, c * t1 + d]); }
+                }
+                else if(D > 0){
+                    t1 = -(B + Math.sqrt(D)) / (2 * A);
+                    t2 = -(B - Math.sqrt(D)) / (2 * A);
+
+                    if(t1 >= 0 && t1 <= 1){ num_t++; intersections.push([a * t1 + b, c * t1 + d]); }
+                    if(t2 >= 0 && t2 <= 1){ num_t++; intersections.push([a * t2 + b, c * t2 + d]); }
+                }
+
+                switch(num_t){
+                    case 0:
+                        setupText(canvas, "No intersection", 3);
+                        break;
+                    case 1:
+                        setupText(canvas, "Intersection Points: 1 Point 1: (" + intersections[0][0].toFixed(2) + ", " + intersections[0][1].toFixed(2) + ")", 3);
+                        break;
+                    case 2:
+                        setupText(canvas, "Intersection Points: 2 Point 1: (" + intersections[0][0].toFixed(2) + ", " + intersections[0][1].toFixed(2) +
+                            ") Point 2: (" + intersections[1][0].toFixed(2) + ", " + intersections[1][1].toFixed(2) + ")", 3);
+                        break;
+                }
+            }
+
+            isDrawing = false;
+            startPoint = null;
+            tempEndPoint = null;
+            render();
+        }
+    }
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+}
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    shader.use();
+    
+    // Draw line
+    for (let line of lines) {
+        shader.setVec4("u_color", [0.5, 0.5, 1.0, 1.0]);
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(line), gl.STATIC_DRAW);
+        gl.bindVertexArray(vao);
+        gl.drawArrays(gl.LINES, 0, 2);
+    }
+
+    // ...or circle
+    for (let circle of circles) {
+        shader.setVec4("u_color", [1.0, 0.0, 1.0, 1.0]);
+
+        let r = Math.sqrt(Math.pow(circle[0] - circle[2], 2) + Math.pow(circle[1] - circle[3], 2));
+        let circleCentX = circle[0]; let circleCentY = circle[1];
+        let circleVert = [];
+        for(let i = 0; i < CIRCLE_PRECISION; i++){
+            circleVert.push(circleCentX + Math.cos(Math.PI * 2 / CIRCLE_PRECISION * i) * r);
+            circleVert.push(circleCentY + Math.sin(Math.PI * 2 / CIRCLE_PRECISION * i) * r);
+        }
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(circleVert), gl.STATIC_DRAW);
+        gl.bindVertexArray(vao);
+        gl.drawArrays(gl.LINE_LOOP, 0, CIRCLE_PRECISION);
+    }
+
+    for (let intersection of intersections) {
+        shader.setVec4("u_color", [1.0, 1.0, 0.0, 1.0]);
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(intersection), gl.STATIC_DRAW);
+        gl.bindVertexArray(vao);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+
+    // 임시 선 그리기
+    if (isDrawing && startPoint && tempEndPoint) {
+        if(shape == SHAPE.CIRCLE){
+            shader.setVec4("u_color", [0.5, 0.5, 0.5, 1.0]); // 임시 선분의 color는 회색
+
+            let r = Math.sqrt(Math.pow(startPoint[0] - tempEndPoint[0], 2) + Math.pow(startPoint[1] - tempEndPoint[1], 2));
+            let circleCentX = startPoint[0]; let circleCentY = startPoint[1];
+            let circleVert = [];
+            for(let i = 0; i < CIRCLE_PRECISION; i++){
+                circleVert.push(circleCentX + Math.cos(Math.PI * 2 / CIRCLE_PRECISION * i) * r);
+                circleVert.push(circleCentY + Math.sin(Math.PI * 2 / CIRCLE_PRECISION * i) * r);
+            }
+
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(circleVert), 
+                          gl.STATIC_DRAW);
+            gl.bindVertexArray(vao);
+            gl.drawArrays(gl.LINE_LOOP, 0, CIRCLE_PRECISION);
+        }
+        else if(shape == SHAPE.LINE){
+            shader.setVec4("u_color", [0.5, 0.5, 0.5, 1.0]); // 임시 선분의 color는 회색
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([...startPoint, ...tempEndPoint]), 
+                          gl.STATIC_DRAW);
+            gl.bindVertexArray(vao);
+            gl.drawArrays(gl.LINES, 0, 2);
+        }
+    }
+
+    // axes 그리기
+    axes.draw(mat4.create(), mat4.create()); // 두 개의 identity matrix를 parameter로 전달
+}
+
+async function initShader() {
+    const vertexShaderSource = await readShaderFile('shVert.glsl');
+    const fragmentShaderSource = await readShaderFile('shFrag.glsl');
+    shader = new Shader(gl, vertexShaderSource, fragmentShaderSource);
+}
+
+async function main() {
+    try {
+        if (!initWebGL()) {
+            throw new Error('WebGL 초기화 실패');
+            return false; 
+        }
+
+        // 셰이더 초기화
+        await initShader();
+        
+        // 나머지 초기화
+        setupBuffers();
+        shader.use();
+        
+        // 마우스 이벤트 설정
+        setupMouseEvents();
+        
+        // 초기 렌더링
+        render();
+
+        return true;
+        
+    } catch (error) {
+        console.error('Failed to initialize program:', error);
+        alert('프로그램 초기화에 실패했습니다.');
+        return false;
+    }
+}
